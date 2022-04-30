f6b2abe5a0928f7d2c7c65e08a443ac8895686d6
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-04-30 15:34:39 -0300

    Added effects and added session services

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,14 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
