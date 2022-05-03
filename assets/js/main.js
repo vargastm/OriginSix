ccc8fb374d3ff2d25b743bb491d729f318457d48
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-05-02 22:18:42 -0300

    Finished responsability

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,31 @@ scrollReveal.reveal(
   `,
   { interval: 100 }
 )
+
+/* ACTIVE SECTION */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  activateMenuAtCurrentSection()
+})
